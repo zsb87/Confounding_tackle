faeda818d1a058f74b38d7a45567560a7b63c819
zsb87 <zhangshibo87@gmail.com>
2018-06-14 09:32:50 -0500

    paper submission

--- classification.py
+++ classification.py
@@ -1,10 +1,11 @@
 # try 1: remove dd, drinking may assemble smoking more, and different from feeding
+# try 2: generate fake data, the 10 fold results vary, maybe the data set is too small.
+# 
 
 from __future__ import division
 import time
 import datetime
 import csv
-from sklearn import svm, neighbors, metrics, cross_validation, preprocessing
 from sklearn.externals import joblib
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, AdaBoostClassifier
@@ -30,7 +31,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import scipy.io as sio
 from collections import Counter
-from sklearn import preprocessing
+from sklearn import preprocessing, svm
 from sklearn.metrics import matthews_corrcoef
 import numpy.polynomial.polynomial as poly
 pd.set_option('display.max_rows', 500)
@@ -41,10 +42,30 @@ from sklearn.metrics import *
 from sklearn.metrics import precision_recall_fscore_support as score
 from datetime import timedelta
 from pylatex import Document, LongTable, MultiColumn
-from utils import tt_split_pseudo_rand, calc_multi_cm, balanced_subsample
+from utils import tt_split_pseudo_rand, calc_multi_cm, balanced_subsample, wacc_from_CM
 
 import warnings
 warnings.filterwarnings("ignore")
+
+from scipy import interp
+from sklearn.metrics import auc, roc_curve
+from sklearn.model_selection import StratifiedKFold,KFold
+
+from imblearn.over_sampling import ADASYN, SMOTE, RandomOverSampler
+from imblearn.pipeline import make_pipeline
+
+
+
+def standard_normalizer(x):
+    x = x.T
+    # compute the mean and standard deviation of the input
+    x_means = np.mean(x,axis = 1)[:,np.newaxis]
+    x_stds = np.std(x,axis = 1)[:,np.newaxis]   
+    # create standard normalizer function based on input data statistics
+    normalizer = lambda data: ((data.T - x_means)/x_stds).T
+    # return normalizer and inverse_normalizer
+    return normalizer
+
 
 
 
@@ -58,46 +79,94 @@ fd,dd=pd.read_csv(fd_file),pd.read_csv(dd_file)
 sd,cd,null=pd.read_csv(sd_file),pd.read_csv(cd_file),pd.read_csv(null_file)
 
 pos=pd.concat([fd,dd])
-neg=pd.concat([sd,cd,null])
+# neg=pd.concat([sd,cd,null])
+neg = null
 
 pos['label']=1
 neg['label']=0
 posneg = pd.concat([pos,neg])
+XY_all = posneg.as_matrix()
+# print(XY_all)
 
-XY_trn, XY_test = tt_split_pseudo_rand(posneg.as_matrix(), .7, seed=1)
-X_trn, Y_trn = XY_trn[:,:-1], XY_trn[:,-1]
-X_test, Y_test = XY_test[:,:-1], XY_test[:,-1]
+# XY_trn, XY_test = tt_split_pseudo_rand(, .7, seed=1)
 
-X_trn, Y_trn = balanced_subsample(X_trn,Y_trn,subsample_size=1.0)
+fsc_list = []
 
-# print( "Shape of training data: " + str(X_trn.shape))
-# print( "Shape of training label: " + str(Y_trn.shape))
+skf = StratifiedKFold(n_splits=10)
 
-# pos_examples_counter = len(np.where(Y_trn == 1)[0])
-# neg_examples_counter = len(np.where(Y_trn == 0)[0])
+first_flag = 1
 
-# print( "Positive Examples: " + str(pos_examples_counter))
-# print( "Negative Examples: " + str(neg_examples_counter))
+for XY_trn_idx, XY_test_idx in skf.split(XY_all[:,:-1], XY_all[:,-1]):
+    XY_trn = XY_all[XY_trn_idx]
+    XY_test = XY_all[XY_test_idx]
 
-# Train AdaBoostClassifier
-#clf = ExtraTreesClassifier(n_estimators=100)
-clf = RandomForestClassifier(n_estimators=185)
-#clf = AdaBoostClassifier(n_estimators=185)
-#clf = KNeighborsClassifier(n_neighbors=2)
-#clf = svm.LinearSVC()
-#clf = GaussianNB()
-#clf = DecisionTreeClassifier()
-#clf = LogisticRegression()
+    X_trn, Y_trn = XY_trn[:,:-1], XY_trn[:,-1]
+    X_test, Y_test = XY_test[:,:-1], XY_test[:,-1]
 
-clf.fit(X_trn,Y_trn)
 
-X_test = XY_test[:,:-1]
-Y_test = XY_test[:,-1]
-predicted = clf.predict(X_test)
+    # return normalization functions based on input x
+    normalizer = standard_normalizer(X_trn)
+    # normalize input by subtracting off mean and dividing by standard deviation
+    X_trn = normalizer(X_trn)
+    X_test = normalizer(X_test)
 
-# print("Shape of Predicted: " + str(predicted.shape))
 
-result, cm = calc_multi_cm(Y_test, predicted)
+    # X_trn, Y_trn = balanced_subsample(X_trn,Y_trn,subsample_size=1.0)
+    X_trn,Y_trn = SMOTE().fit_sample(X_trn,Y_trn)
+    X_trn,Y_trn = ADASYN().fit_sample(X_trn,Y_trn)
+
+    # print( "Shape of training data: " + str(X_trn.shape))
+    # print( "Shape of training label: " + str(Y_trn.shape))
+
+    # pos_examples_counter = len(np.where(Y_trn == 1)[0])
+    # neg_examples_counter = len(np.where(Y_trn == 0)[0])
+
+    # print( "Positive Examples: " + str(pos_examples_counter))
+    # print( "Negative Examples: " + str(neg_examples_counter))
+
+    # clf = ExtraTreesClassifier(n_estimators=100)
+    clf = RandomForestClassifier(n_estimators=185)
+    # clf = AdaBoostClassifier(n_estimators=185)
+    # clf = KNeighborsClassifier(n_neighbors=2)
+    # clf = svm.LinearSVC()
+    #clf = GaussianNB()
+    # clf = DecisionTreeClassifier()
+    # clf = LogisticRegression()
+
+    clf.fit(X_trn,Y_trn)
+
+    X_test = XY_test[:,:-1]
+    Y_test = XY_test[:,-1]
+    predicted = clf.predict(X_test)
+
+    # print("Shape of Predicted: " + str(predicted.shape))
+
+    result, cm = calc_multi_cm(Y_test, predicted)
+    # print(Y_test)
+    # print(predicted.shape)
+
+    if first_flag:
+    	Y_test_all = Y_test
+    	predicted_all = predicted
+    	first_flag = 0
+    else:
+    	Y_test_all = np.hstack((Y_test_all, Y_test))
+    	predicted_all = np.hstack((predicted_all, predicted))
+
+
+    print(result['1']['f1'])
+    print(cm)
+    # fsc_list.append(result['1']['f1'])
+
+
+# print(np.mean(np.array(fsc_list)))
+result, cm = calc_multi_cm(Y_test_all, predicted_all)
+print(result['1']['f1'])
 print(result)
 print(cm)
+
+
+
+
+# print(wacc_from_CM(cm, 2))
 
--- t-sne.py
+++ t-sne.py
@@ -4,6 +4,7 @@ import os,time
 from sklearn.manifold import TSNE
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from imblearn.over_sampling import ADASYN, SMOTE, RandomOverSampler
 
 
 
@@ -16,20 +17,35 @@ null_folder='All/null/null_groundtruthsegmentation.csv'
 fd,dd=pd.read_csv(fd_folder),pd.read_csv(dd_folder)
 sd,cd,null=pd.read_csv(sd_folder),pd.read_csv(cd_folder),pd.read_csv(null_folder)
 
-# fd_dd=pd.concat([fd,dd])
-fd_dd = fd
-sd_cd=pd.concat([sd,cd,null])
+fd_dd=pd.concat([fd,dd])
+# fd_dd = fd
+# sd_cd=pd.concat([sd,cd,null])
+sd_cd_null=pd.concat([sd,cd,null])
+
+sd['label']=2
 fd_dd['label']=1
-sd_cd['label']=0
-together=pd.concat([fd_dd,sd_cd])
+null['label']=0
+
+together=pd.concat([fd_dd,sd,null])
+
+X = together.values[:,:-1]
+y = together.values[:,-1]
+names = together.columns
+
+# X,y = SMOTE().fit_sample(X,y)	
+
+## save to file
+# together = pd.DataFrame(data = np.hstack((X,y[:,np.newaxis])),columns=names)
+# together.to_csv('All/fd_dd_null_groundtruthsegmentation_SMOTE.csv',index=None)
 
 time_start = time.time()
 tsne = TSNE(n_components=3, verbose=1, perplexity=30,n_iter=1000)
-data_tsne=tsne.fit_transform(together.values[:,:-1])
+data_tsne=tsne.fit_transform(X)
 print ('t-SNE done! Time elapsed: {} seconds'.format(time.time()-time_start))
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(data_tsne[:,0], data_tsne[:,1],data_tsne[:,2], c=together.values[:,-1])
+ax.scatter(data_tsne[:,0], data_tsne[:,1],data_tsne[:,2], c=y)
 # plt.colorbar(ticks=range(2))
 plt.show()
+
